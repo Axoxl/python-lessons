292b1d23a2057a4ce35bee608b4b50e10ca0f1ac
Axoxl <89534626+Axoxl@users.noreply.github.com>
2021-09-30 10:22:39 +0300

    Update 2.5.py

--- урок 2/2.5.py
+++ урок 2/2.5.py
@@ -1,13 +1,10 @@
-list1 = [2, 9, 1, 5]
-a = input("введите целое натуральное число")
-for i in list1:
-    if a == 2:
-        print(list1.insert(1, a))
-    if a == 9:
-        print(list1.insert(2, a))
-    if a == 1:
-        print(list1.insert(3, a))
-    if a == 5:
-        print(list1.insert(4, a))
-    else:
-        print(list1, a)
+rating = [9, 5, 1, 2]
+new_element = int(input('Пожалуйста введите новый элемент рейтинга: '))
+counter = rating.count(new_element)
+if counter:
+    index = rating.index(new_element)
+    rating.insert(index + counter, new_element)
+else:
+    rating.append(new_element)
+    rating.sort(reverse=True)
+print(rating)
